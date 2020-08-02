7b27a441d4b68dfad5c7f29697d57294254b3c8f
alexiahsu <hsu.enyang@gmail.com>
2020-08-02 11:38:14 +0800

    added cloudinary route to to create_volunteer

--- app.py
+++ app.py
@@ -15,23 +15,33 @@ MONGO_URI = os.environ.get('MONGO_URI')
 client = pymongo.MongoClient(MONGO_URI)
 db = client.tgc_project3
 
+# Connect to Cloudinary
+CLOUD_NAME = os.environ.get('CLOUD_NAME')
+UPLOAD_PRESET = os.environ.get('UPLOAD_PRESET')
+
 # Session Key
 SESSION_KEY = os.environ.get('SESSION_KEY')
 app.secret_key = SESSION_KEY
 
 # Homepage (open access)
+
+
 @app.route('/')
 def index():
     vol_listings = db.volunteer_prog.find().limit(1)
     return render_template('index.template.html', vol_listings=vol_listings)
 
 # Event list (open access)
+
+
 @app.route('/volunteer')
 def show_volunteer():
     vol_listings = db.volunteer_prog.find()
     return render_template('volunteer.template.html', vol_listings=vol_listings)
 
 # Individual Event (open access)
+
+
 @app.route('/volunteer/signup/<activity_id>')
 def show_activity_details(activity_id):
     vol = db.volunteer_prog.find_one({
@@ -39,35 +49,41 @@ def show_activity_details(activity_id):
     })
     return render_template('activity_signup.html', vol=vol)
 
+
 @app.route('/volunteer/details/<activity_id>', methods=['POST'])
 def process_activity_volunteer(activity_id):
-    vol_name= request.form.get('vol_name')
+    vol_name = request.form.get('vol_name')
     vol_dob = request.form.get('vol_dob')
     vol_email = request.form.get('vol_email')
     vol_phone = request.form.get('vol_phone')
     vol_desc = request.form.get('vol_desc')
     vol_activity = activity_id
     db.volunteer_register.insert_one({
-    'vol_name': vol_name,
-    'vol_dob': datetime.datetime.strptime(vol_dob, "%Y-%m-%d"),
-    'vol_desc': vol_desc,
-    'vol_email': vol_email,
-    'vol_activity': vol_activity,
-    'vol_phone': vol_phone
+        'vol_name': vol_name,
+        'vol_dob': datetime.datetime.strptime(vol_dob, "%Y-%m-%d"),
+        'vol_desc': vol_desc,
+        'vol_email': vol_email,
+        'vol_activity': vol_activity,
+        'vol_phone': vol_phone
     })
     flash(f'You have registered for the event!')
     return render_template('volunteer.template.html')
 
 # Participating organisations (open access)
+
+
 @app.route('/organisations')
 def show_organisations():
     users = db.users.find()
     return render_template('organisations.template.html', users=users)
 
 # Make donations page (open access)
+
+
 @app.route('/donate')
 def show_donate_form():
     return render_template('make_donation.template.html')
+
 
 @app.route('/donate', methods=['POST'])
 def donate_form():
@@ -80,29 +96,35 @@ def donate_form():
     donor_memo = request.form.get('donor_message')
 
     db.donation.insert_one({
-    'donor_name': donor_name,
-    'donor_dob': datetime.datetime.strptime(donor_dob, "%Y-%m-%d"),
-    'donor_email': donor_email,
-    'donor_phone': donor_phone,
-    'donor_amount': donor_amount,
-    'payment_method': payment_method,
-    'donor_memo': donor_memo
+        'donor_name': donor_name,
+        'donor_dob': datetime.datetime.strptime(donor_dob, "%Y-%m-%d"),
+        'donor_email': donor_email,
+        'donor_phone': donor_phone,
+        'donor_amount': donor_amount,
+        'payment_method': payment_method,
+        'donor_memo': donor_memo
     })
     flash(f'Thank you for donating {donor_amount} to us!')
     return render_template('index.template.html')
 
 # Login/Register Pages
+
+from user import routes
+from functools import wraps
+
+
 @app.route('/login')
 def login_page():
     return render_template('login.template.html')
+
 
 @app.route('/register')
 def register_page():
     return render_template('signup.template.html')
 
+
 # Setting up login function
-from functools import wraps
-from user import routes
+
 
 def login_required(f):
     @wraps(f)
@@ -114,6 +136,8 @@ def login_required(f):
     return wrap
 
 # Events dashboard
+
+
 @app.route('/dashboard')
 @login_required
 def dashboard():
@@ -121,6 +145,8 @@ def dashboard():
     return render_template('events_dashboard.html', vol_listings=vol_listings)
 
 # Update event
+
+
 @app.route('/volunteer/update/<activity_id>')
 @login_required
 def update_event(activity_id):
@@ -132,6 +158,7 @@ def update_event(activity_id):
 
     return render_template('update_event.html', event=event, all_users=all_users)
 
+
 @app.route('/volunteer/update/<activity_id>', methods=['POST'])
 @login_required
 def process_update_event(activity_id):
@@ -141,6 +168,7 @@ def process_update_event(activity_id):
     duration = request.form.get('duration')
     num_volunteer_req = request.form.get('num_volunteer_req')
     prog_date = request.form.get('prog_date')
+    uploaded_file_url = request.form.get('uploaded_file_url')
 
     db.volunteer_prog.update_one({
         "_id": ObjectId(activity_id)
@@ -151,12 +179,15 @@ def process_update_event(activity_id):
             "prog_org": prog_org,
             "duration": duration,
             "num_volunteer_req": num_volunteer_req,
-            "prog_date": prog_date
+            "prog_date": prog_date,
+            'uploaded_file_url': uploaded_file_url
         }
     })
     return redirect(url_for('dashboard'))
 
 # Delete event
+
+
 @app.route('/volunteer/delete/<activity_id>')
 @login_required
 def delete_event(activity_id):
@@ -165,6 +196,7 @@ def delete_event(activity_id):
     })
 
     return render_template('confirm_delete_event.html', vol=vol)
+
 
 @app.route('/volunteer/delete/<activity_id>', methods=['POST'])
 @login_required
@@ -175,11 +207,15 @@ def process_delete_event(activity_id):
     return redirect(url_for('dashboard'))
 
 # Create event volunteer
+
+
 @app.route('/volunteer/create')
 @login_required
 def create_volunteer_form():
     all_users = db.users.find()
-    return render_template('create_volunteer.template.html', all_users=all_users)
+    return render_template('create_volunteer.template.html', all_users=all_users,
+                           cloud_name=CLOUD_NAME, upload_preset=UPLOAD_PRESET)
+
 
 @app.route('/volunteer/create', methods=['POST'])
 @login_required
@@ -190,15 +226,16 @@ def create_volunteer():
     duration = request.form.get('duration')
     num_volunteer_req = request.form.get('num_volunteer_req')
     prog_date = request.form.get('prog_date')
+    uploaded_file_url = request.form.get('uploaded_file_url')
     db.volunteer_prog.insert_one({
         'prog_name': prog_name,
         'prog_date': datetime.datetime.strptime(prog_date, "%Y-%m-%d"),
         'prog_desc': prog_desc,
         'duration': duration,
         'num_volunteer_req': num_volunteer_req,
-        'prog_org': prog_org
+        'prog_org': prog_org,
+        'uploaded_file_url': uploaded_file_url
     })
-    flash(f'Your event has been created!')
     return redirect(url_for('dashboard'))
 
 
